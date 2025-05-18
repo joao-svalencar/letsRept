d017a37ee5aad7ab5b6c4a828dd1fdea8b6cc9d7
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-05-18 13:24:32 -0300

    implemented full binomial search, with printed content

--- R/herpSearch.R
+++ R/herpSearch.R
@@ -19,18 +19,54 @@
 
 herpSearch <- function(binomial=NULL, highertaxa=NULL, genus=NULL, distribution=NULL){
 
-  # block for single species search -----------------------------------------
-  #for binomial:
-  if(!is.null(binomial))
+# block for single species search -----------------------------------------
+#for binomial:
+if(!is.null(binomial))
+{
+  base_url <- "https://reptile-database.reptarium.cz/species"
+  gen <- strsplit(binomial, " ")[[1]][1]
+  species <- strsplit(binomial, " ")[[1]][2]
+  query <- paste0("?genus=", gen, "&species=", species)
+  url <- paste0(base_url, query) #url for direct species search
+  
+  element <- rvest::html_element(url, "table") #get the content table for each species
+  
+  #pasting the content of Higher Taxa, Subspecies, Common Names, Synonyms, Distribution and Reproduction when available:
+  for (i in 1:6)
   {
-    base_url <- "https://reptile-database.reptarium.cz/species"
-    genus <- strsplit(binomial, " ")[[1]][1]
-    species <- strsplit(binomial, " ")[[1]][2]
-    query <- paste0("?genus=", genus, "&species=", species)
-    url <- paste0(base_url, query) #url for direct species search
+    row <- xml2::xml_child(element, i)
+    cells <- xml2::xml_children(row)
     
-    #paste the scrapping for one species code
-  }
+    if (length(cells) >= 2) {
+      title <- rvest::html_text(cells[1], trim = TRUE)
+      content_raw <- rvest::html_text(cells[2], trim = TRUE)
+      
+      if (content_raw == "") next
+      
+      if (i == 4) {
+        # For Synonym row: split by <br> and trim
+        br_items <- cells[2] %>%
+          rvest::html_nodes("br") %>%
+          xml2::xml_add_sibling("marker", "<SPLIT>") # helper tag to split text
+        syn_text <- rvest::html_text(cells[2], trim = TRUE)
+        syn_list <- unlist(strsplit(syn_text, "<SPLIT>"))
+        syn_list <- trimws(syn_list[syn_list != ""])
+        
+        cat("Synonyms:\n")
+        for (item in syn_list) {
+          cat(" -", item, "\n")
+        }
+        cat("\n")
+      } else {
+        content <- rvest::html_text(cells[2], trim = TRUE)
+        cat(paste0(title,":"),"\n")
+        cat(content,"\n\n")
+      }
+    }
+  } #closes the species content printing
+  return(invisible(NULL))
+}else #closes the if binomial part
+  { 
 
 # block for advanced search -----------------------------------------------
   #higher taxa:
@@ -54,6 +90,7 @@ herpSearch <- function(binomial=NULL, highertaxa=NULL, genus=NULL, distribution=
     query <- paste0("?location=", distribution, "&submit=Search")
     url <- paste0(base_url, query) #url for page of species list
     }
-  
-  return(url)
+  return(url) 
+  }
 }
+  
