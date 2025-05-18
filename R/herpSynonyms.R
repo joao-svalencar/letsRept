4f9c5e06c4663fbd317022fc9084a8963e827346
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-05-18 13:28:20 -0300

    fixing scrapping process

--- R/herpSynonyms.R
+++ R/herpSynonyms.R
@@ -21,16 +21,32 @@ herpSynonyms <- function(x)
 {
   species_list <- c()
   synonym_list <- c()
-  synonym_vector_list <- c()
   
   for(i in 1:length(x$species))
   {
-    #add random sleep time
-    Sys.sleep(stats::runif(1, min = 0.3, max = 1)) # random sleep time
+    # try_get_html <- function(url, tries = 3) {
+    #   for (attempt in 1:tries) {
+    #     try({
+    #       response <- httr::GET(url, httr::user_agent("Mozilla/5.0"), timeout(30))
+    #       return(rvest::read_html(response))
+    #     }, silent = TRUE)
+    #     Sys.sleep(runif(1, 1, 3))  # slightly longer pause after failure
+    #   }
+    #   warning(sprintf("Failed to fetch: %s after %d tries", url, tries))
+    #   return(NULL)
+    # }
+    # 
+    # #add random sleep time
+    # Sys.sleep(stats::runif(1, min = 0.3, max = 1)) # random sleep time
+    # 
+    # url <- try_get_html(x$url[i])
+    # if (!is.null(url)) {
+    #   element <- rvest::html_element(url, "table")
+    # }
     
-    url <- rvest::read_html(x$url[i])
+    url <- rvest::read_html(httr::GET(x$url[i], httr::user_agent("Mozilla/5.0")))
     element <- rvest::html_element(url, "table") #scrap species table from Reptile Database
-    
+
     #synonyms
     syn <- xml2::xml_child(element, 4) #select the synonym part of the table
     td2 <- rvest::html_element(syn, "td:nth-child(2)")
