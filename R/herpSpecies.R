9e1bd047f7152815f142b72f67da6ce99277ec7f
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-05-18 14:44:32 -0300

    added message to herpSpecies

--- R/herpSpecies.R
+++ R/herpSpecies.R
@@ -84,13 +84,13 @@ herpSpecies <- function(url, higherTaxa = FALSE)
                                   species = species_list,
                                   url = url_list,
                                   stringsAsFactors = FALSE)
-  cat("Data collection is done!","\n", paste0("A total of ", length(species_list), " species links and higher taxa information retrieved."))
+  cat(" Data collection is done!","\n", paste0("A total of ", length(species_list), " species links and higher taxa information retrieved."))
    
   }else{
   searchResults <- data.frame(species = species_list,
                               url = url_list,
                               stringsAsFactors = FALSE)
-  cat("Data collection is done!","\n", paste0("A total of ", length(species_list), " species links retrieved."))
+  cat(" Data collection is done!","\n", paste0("A total of ", length(species_list), " species links retrieved."))
   }
   
   
