7efd6805d40df140d676fd4a9212b29c40195f9c
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-05-19 01:31:52 -0300

    updated allSynonyms internal dataset

--- R/data_synonyms.R
+++ R/data_synonyms.R
@@ -2,10 +2,11 @@
 #'
 #' This dataset contains the valid names and respective listed synonyms for all reptile species cataloged in The Reptile Database.
 #'
-#' @format A dataframe with XXXXX rows and 2 variables:
+#' @format A dataframe with 109,082 rows and 3 variables:
 #' \describe{
-#'   \item{species}{A character vector with known current valid name for all reptile species cataloged in The Reptile Database website (download: May 15th, 2025)}
+#'   \item{species}{A character vector with known current valid name for all reptile species cataloged in The Reptile Database website (download: May 19th, 2025)}
 #'   \item{synonym}{A character column with the respective synonyms for all reptile species cataloged in The Reptile Database website information (download: May 19th, 2025)}
+#'   \item{ref}{A charater column with the synonyms and respective references that used it}
 #' }
 #' @source The data was sampled from The Reptile Database website https://reptile-database.reptarium.cz using function HerpNom::getSynonyms(HerpNom::allReptiles) 
 #' 
