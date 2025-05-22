32d31ecb192095fb6682e9d56a1e1fe5bd520a00
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-05-21 22:46:56 -0300

    less redundant, faster, allows user data

--- R/herpSpecies.R
+++ R/herpSpecies.R
@@ -30,17 +30,13 @@
 #' 
 #' @export
 #'
-herpSpecies <- function(url, taxonomicInfo = TRUE, fullHigher = FALSE, getLink = FALSE, batches = 1, startBatch = 1)
+herpSpecies <- function(url, dataList = NULL, taxonomicInfo = TRUE, fullHigher = FALSE, getLink = FALSE, batches = 1, startBatch = 1)
 {
+  if(is.null(dataList))
+  {
   species_list <- c()
   genus_list <- c()
   url_list <- c()
-  taxa_vector_list <- c()
-  order_list <- c()
-  suborder_list <- c()
-  family_list <- c()
-  sppAuthor_list <- c()
-  sppYear_list <- c()
   
   search <- rvest::read_html(url)
   ul_element <- rvest::html_elements(search, "#content > ul:nth-child(6)")
@@ -50,32 +46,62 @@ herpSpecies <- function(url, taxonomicInfo = TRUE, fullHigher = FALSE, getLink =
     #add random sleep time
     Sys.sleep(stats::runif(1, min = 0.3, max = 1)) # random sleep time
     
-    target <- xml2::xml_child(xml2::xml_child(ul_element[[1]], i), 1)
+    li_node <- xml2::xml_child(ul_element[[1]], i)
+    target <- xml2::xml_child(li_node, 1)
     
     species <- rvest::html_element(target, "em") |> rvest::html_text(trim = TRUE)
     genus <- sub(" .*", "", species)
-    url <- paste("https://reptile-database.reptarium.cz", xml2::xml_attrs(target)[["href"]], sep="")
+    sppLink <- paste("https://reptile-database.reptarium.cz", xml2::xml_attrs(target)[["href"]], sep="")
     
     species_list <- c(species_list, species)
     genus_list <- c(genus_list, genus)
-    url_list <- c(url_list, url)
+    url_list <- c(url_list, sppLink)
     
     percent <- (i/length(xml2::xml_children(ul_element[[1]]))) * 100
     cat(sprintf("\rGetting species links progress: %.1f%%", percent))
     utils::flush.console()
   }
   cat("\n")
-    #code with batch:
+  
+  if(taxonomicInfo == FALSE) {
+    if(getLink == TRUE){
+      searchResults <- data.frame(species = species_list,
+                                  url = url_list,
+                                  stringsAsFactors = FALSE)
+
+      message_text <- paste0("A total of ", length(species_list), " species links retrieved.")
+      
+    }else{
+      searchResults <- species_list
+      
+      message_text <- paste0("A total of ", length(species_list), " species retrieved.")
+    }
+    cat(" Data collection is done!\n", message_text, "\n")
+    return(searchResults)
+    }
+  }else{
+    species_list <- dataList$species
+    genus_list <- sub(" .*", "", species_list)
+    url_list <- dataList$url
+# taxonomicInfo == TRUE ---------------------------------------------------
+  #code with batch:
   if (taxonomicInfo == TRUE) {
+    
+    taxa_vector_list <- c()
+    order_list <- c()
+    suborder_list <- c()
+    family_list <- c()
+    sppAuthor_list <- c()
+    sppYear_list <- c()
+    
+    orders <- c("Squamata", "Crocodylia", "Rhychocephalia", "Testudines")
+    suborders <- c("Sauria", "Serpentes")
     
     match_taxon <- function(taxa_vector, rank_list) {
       match_idx <- which.max(vapply(rank_list, function(rank) any(stringr::str_detect(taxa_vector, rank)), logical(1)))
       result <- rank_list[match_idx]
       if (identical(result, character(0))) NA else result
     }
-    
-    orders <- c("Squamata", "Crocodylia", "Rhychocephalia", "Testudines")
-    suborders <- c("Sauria", "Serpentes")
     
     # Default values if not provided
     if (is.null(batches)) batches <- 1
@@ -86,7 +112,7 @@ herpSpecies <- function(url, taxonomicInfo = TRUE, fullHigher = FALSE, getLink =
     
     success <- TRUE
     
-    for (b in startBatch:batches) {
+    for(b in startBatch:batches){
       from <- ((b - 1) * batch_size) + 1
       to <- min(b * batch_size, total_species)
       
@@ -95,7 +121,6 @@ herpSpecies <- function(url, taxonomicInfo = TRUE, fullHigher = FALSE, getLink =
       batch_success <- tryCatch({
         for (j in from:to) {
           sp_page <- rvest::read_html(url_list[j])
-          
           title <- rvest::html_element(sp_page, "h1")
           
           sppAuthor <- rvest::html_text(title, trim = TRUE)
@@ -133,12 +158,12 @@ herpSpecies <- function(url, taxonomicInfo = TRUE, fullHigher = FALSE, getLink =
         return(FALSE)
       })
       
-      if (!batch_success) break
+      if(!batch_success)break
     }
     
     # Final output — always return as much as was scraped
     n <- length(family_list)
-    searchResults <- data.frame(
+    searchResults <-  data.frame(
                       order = order_list,
                       suborder = suborder_list,
                       family = family_list,
@@ -149,132 +174,12 @@ herpSpecies <- function(url, taxonomicInfo = TRUE, fullHigher = FALSE, getLink =
                       stringsAsFactors = FALSE
                       )
     
-    if (getLink == TRUE) {
-      searchResults$url <- url_list[1:n]
+      if (getLink == TRUE) 
+      {
+        searchResults$url <- url_list[1:n]
+      }
+    
     }
-    
-    return(searchResults)
-  }
-  
-  #if(taxonomicInfo== TRUE){
-    # # to get higher taxa information ------------------------------------------
-    # match_taxon <- function(taxa_vector, rank_list) {
-    #   match_idx <- which.max(vapply(rank_list, function(rank) any(stringr::str_detect(taxa_vector, rank)), logical(1)))
-    #   result <- rank_list[match_idx]
-    #   if (identical(result, character(0))) NA else result
-    # }
-    # 
-    # orders <- c("Squamata", "Crocodylia", "Rhychocephalia", "Testudines") #order count = OK
-    # suborders <- c("Sauria", "Serpentes") #suborder count = OK
-    # 
-    # if (is.null(num_batches)) num_batches <- 1
-    # if (is.null(start_batch)) start_batch <- 1
-    # 
-    # total_species <- length(species_list)
-    # batch_size <- ceiling(total_species / num_batches)
-    
-    # species_links <- tryCatch({
-    #   for(j in 1:length(species_list))
-    #   {
-    #     sp_page <- rvest::read_html(url_list[j])
-    #     element <- rvest::html_element(sp_page, "table") #scrap species table from Reptile Database
-    #     taxa <- xml2::xml_child(element, 1) #select the higher taxa part of the table
-    #     td_taxa <- rvest::html_element(taxa, "td:nth-child(2)")
-    #     children <- xml2::xml_contents(td_taxa)
-    #     
-    #     #each species Higher Taxa information:
-    #     taxa_vector <- children[xml2::xml_name(children) == "text"] |> rvest::html_text(trim = TRUE)
-    #     family <- stringr::str_extract(taxa_vector, "\\b[A-Z][a-z]+idae\\b")
-    #     order <- match_taxon(taxa_vector, orders)
-    #     #order <- orders[stringr::str_detect(taxa_vector, orders)][1] #get respective item from orders
-    #     suborder <- match_taxon(taxa_vector, suborders)
-    #     #suborder <- suborders[stringr::str_detect(taxa_vector, suborders)][1] #get respective item from suborders
-    #     
-    #     taxa_vector_list <- c(taxa_vector_list, taxa_vector)
-    #     order_list <- c(order_list, order)
-    #     suborder_list <- c(suborder_list, suborder)
-    #     family_list <- c(family_list, family)
-    #     
-    #     cat("\n")
-    #     percent <- (j/length(species_list)) * 100
-    #     cat(sprintf("Getting higher taxa progress: %.1f%%", percent))
-    #     flush.console()
-    #   }
-    #   
-    # }, error = function(e){
-    #   
-    #   if(getLink==TRUE){
-    #     message("\n Higher taxa scraping failed. Returning only species and links.")
-    #     data.frame(
-    #       species = species_list,
-    #       genus = genus_list,
-    #       url = url_list,
-    #       stringsAsFactors = FALSE
-    #     )
-    #   }else{
-    #     message("\n Higher taxa scraping failed. Returning only species list.")
-    #     data.frame(
-    #       species = species_list,
-    #       genus = genus_list,
-    #       stringsAsFactors = FALSE
-    #     )
-    #   }
-    #   
-    # })# closes tryCatch
-    # return(species_links)
-    # }
-  #}#closes higher taxa = TRUE
-  
-# getLink == FALSE --------------------------------------------------------
-   if(taxonomicInfo==FALSE && getLink==FALSE)
-   { 
-      searchResults <- species_list
-      cat(" Data collection is done!","\n", paste0("A total of ", length(species_list), " species retrieved."))
-      return(searchResults)
-      
-   }else if (taxonomicInfo==TRUE && getLink==FALSE)
-     {
-       n <- length(family_list)
-       searchResults <- data.frame(order = order_list,
-                                   suborder = suborder_list,
-                                   family = family_list,
-                                   genus = genus_list[1:n],
-                                   species = species_list[1:n],
-                                   year = sppYear_list[1:n],
-                                   author = sppAuthor_list[1:n],
-                                   stringsAsFactors = FALSE)
-       if(fullHigher==TRUE)
-       {
-         searchResults$higher_taxa <- taxa_vector_list
-       }
-       cat(" Data collection is done!", "\n", paste0("A total of ", length(species_list), " species higher taxa information retrieved."))
-       return(searchResults)
-     }
-
-# getLink == TRUE ---------------------------------------------------------
-     if(taxonomicInfo ==FALSE && getLink==TRUE)
-     {
-       searchResults <- data.frame(species = species_list,
-                                   url = url_list)
-       cat(" Data collection is done!","\n", paste0("A total of ", length(species_list), " species links retrieved."))
-       return(searchResults)
-     }else if (taxonomicInfo==TRUE && getLink==TRUE)
-       {
-       n <- length(family_list)
-       searchResults <- data.frame(order = order_list,
-                                   suborder = suborder_list,
-                                   family = family_list,
-                                   genus = genus_list[1:n],
-                                   species = species_list[1:n],
-                                   year = sppYear_list[1:n],
-                                   author = sppAuthor_list[1:n],
-                                   url = url_list[1:n],
-                                   stringsAsFactors = FALSE)
-         if(fullHigher==TRUE)
-         {
-          searchResults$higher_taxa <- taxa_vector_list
-         }
-       cat(" Data collection is done!","\n", paste0("A total of ", length(species_list), " species links and higher taxa information retrieved."))
-       return(searchResults)
-     }
+  }return(searchResults)
 }
+  
