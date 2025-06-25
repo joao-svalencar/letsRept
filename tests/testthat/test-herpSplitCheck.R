7faae04107f29eb74fddc4a4d690611015763354
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-06-25 12:25:27 -0300

    added test to herpSplitCheck

---
+++ tests/testthat/test-herpSplitCheck.R
@@ -0,0 +1,14 @@
+test_that("herpSplitCheck returns expected output", {
+  
+  query <- c("Vieira-Alencar authoristicus",
+             "Tantilla melanocephala",
+             "Oxybelis aeneus",
+             "Apostolepis dimidiata",
+             "Bothrops pauloensis")
+  
+  result <- herpSplitCheck(x=query, pubDate = 2019, cores = 2, showProgress = FALSE)
+  
+  expect_s3_class(result, "data.frame")
+  expect_true("RDB" %in% names(result))
+  expect_gt(nrow(result), 0)
+})
